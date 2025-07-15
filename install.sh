7b26c6a24e38bab7d3b0b91bb6dddaeef5a25fe7
hello-im-404 <frankebovich@gmail.com>
2025-07-15 08:57:18 -0400

    update

--- install.sh
+++ install.sh
@@ -27,7 +27,11 @@ sudo mv /usr/share/peass/winpeas/winPEAS.bar ~/
 
 # packets
 
-sudo apt update && sudo apt install -y neovim subfinder gimp nuclei screenfetch golang-go torbrowser-launcher timeshift
+sudo apt update && sudo apt install -y neovim subfinder gimp nuclei screenfetch torbrowser-launcher timeshift git build-essential apt-utils cmake libfontconfig1 libglu1-mesa-dev libgtest-dev libspdlog-dev libboost-all-dev libncurses5-dev libgdbm-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev mesa-common-dev qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5websockets5 libqt5websockets5-dev qtdeclarative5-dev golang-go qtbase5-dev libqt5websockets5-dev python3-dev libboost-all-dev mingw-w64 nasm
+
+# havoc
+
+git clone https://github.com/HavocFramework/Havoc.git
 
 # sliver
 
